b3fe309aa0a37f28a3b6f06688c1d4fdc90cfe46
HalfFocused <82987361+HalfFocused@users.noreply.github.com>
2024-07-01 22:16:49 -0400

    amber medallion

--- src/main/java/code/patches/MakeCopyPatch.java
+++ src/main/java/code/patches/MakeCopyPatch.java
@@ -1,10 +1,17 @@
 package code.patches;
 
+import basemod.cardmods.EtherealMod;
+import basemod.helpers.CardModifierManager;
+import code.relics.AmberMedallion;
 import code.util.charUtil.CardUtil;
 import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import java.util.Iterator;
 
@@ -13,17 +20,15 @@ import java.util.Iterator;
         method="makeStatEquivalentCopy"
 )
 public class MakeCopyPatch {
-
-    @SpireInsertPatch(
-            loc=983,
-            localvars = {"card"}
-    )
-    /*
-    Patch making a copy of a card to also change the rarity if the card's rarity isn't it's default rarity.
-     */
-    public static void Insert(AbstractCard __instance, @ByRef AbstractCard[] card) {
-        if(__instance.rarity != CardUtil.defaultRarity(__instance)) {
-            card[0].rarity = __instance.rarity;
+    public static SpireReturn<AbstractCard> Prefix(AbstractCard __instance) {
+        if(__instance.cardID.equals(Dazed.ID) && AbstractDungeon.isPlayerInDungeon() && AbstractDungeon.player.hasRelic(AmberMedallion.ID) && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT){
+            AbstractCard newCard = AbstractDungeon.returnTrulyRandomCardInCombat().makeCopy();
+            if(!newCard.isEthereal){
+                CardModifierManager.addModifier(newCard, new EtherealMod());
+            }
+            AbstractDungeon.player.getRelic(AmberMedallion.ID).flash();
+            return SpireReturn.Return(newCard);
         }
+        return SpireReturn.Continue();
     }
 }
