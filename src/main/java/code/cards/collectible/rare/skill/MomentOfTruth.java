ff3e5b1e508255216406ecc3271c3957ca06af9c
HalfFocused <82987361+HalfFocused@users.noreply.github.com>
2024-07-22 16:33:58 -0400

    moment of truth

---
+++ src/main/java/code/cards/collectible/rare/skill/MomentOfTruth.java
@@ -0,0 +1,45 @@
+package code.cards.collectible.rare.skill;
+
+import code.cards.AbstractEasyCard;
+
+import static code.ModFile.makeID;
+
+import code.powers.MomentOfTruthPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.unique.ApplyBulletTimeAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class MomentOfTruth extends AbstractEasyCard {
+    public final static String ID = makeID(MomentOfTruth.class.getSimpleName());
+    // intellij stuff skill, self, rare, , , 4, 3, , 
+
+    public MomentOfTruth() {
+        super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
+        isEthereal = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                AbstractPower power = AbstractDungeon.player.getPower(MomentOfTruthPower.POWER_ID);
+                if(power == null){
+                    addToTop(new ApplyPowerAction(p,p, new MomentOfTruthPower(p,1),1));
+                }else{
+                    addToTop(new ApplyBulletTimeAction());
+                    addToTop(new RemoveSpecificPowerAction(p,p, power));
+                }
+                isDone = true;
+            }
+        });
+    }
+
+    public void upp() {
+        isEthereal = false;
+        rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+    }
+}
--- src/main/java/code/powers/MomentOfTruthPower.java
+++ src/main/java/code/powers/MomentOfTruthPower.java
@@ -1,25 +1,19 @@
 package code.powers;
 
 import code.ModFile;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.OnReceivePowerPower;
-import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class LikeClockworkPower extends AbstractEasyPower {
+public class MomentOfTruthPower extends AbstractEasyPower {
     public AbstractCreature source;
 
-    public static final String POWER_ID = ModFile.makeID("LikeClockworkPower");
+    public static final String POWER_ID = ModFile.makeID("MomentOfTruthPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    public LikeClockworkPower(AbstractCreature owner, int amount) {
+    public MomentOfTruthPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
     }
 
